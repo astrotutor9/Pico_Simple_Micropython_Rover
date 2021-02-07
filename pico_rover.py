544545f530bf18507011c7141bc8fff96b8c4728
Robert Wiltshire <37550330+astrotutor9@users.noreply.github.com>
2021-02-07 16:17:05 +0000

    Update pico_rover.py
    Added 5 more LED

--- pico_rover.py
+++ pico_rover.py
@@ -6,13 +6,31 @@ from machine import Pin
 from time import sleep
 
 builtinLed = Pin(25, Pin.OUT)
-red1 = Pin(22,Pin.OUT)
+
+redL = Pin(22,Pin.OUT)
+redR = Pin(9, Pin.OUT)
+
+greenL = Pin(21, Pin.OUT)
+greenR = Pin(10, Pin.OUT)
+
+blueL = Pin(20, Pin.OUT)
+blueR = Pin(11, Pin.OUT)
 
 builtinLed(1)
 
-
 for flashes in range(4):
-    red1(1)
+    redL(1)
+    redR(1)
     sleep(0.5)
-    red1(0)
+    redL(0)
+    redR(0)
+    greenL(1)
+    greenR(1)
     sleep(0.5)
+    greenL(0)
+    greenR(0)
+    blueL(1)
+    blueR(1)
+    sleep(0.5)
+    blueL(0)
+    blueR(0)
