406cbf23059dd7acb7b1ac541ebe06d965f1668c
Robert Wiltshire <37550330+astrotutor9@users.noreply.github.com>
2021-02-07 14:19:28 +0000

    Create pico_rover.py
    Added initial code to start off.

---
+++ pico_rover.py
@@ -0,0 +1,6 @@
+# A simple coding exercise to build a two wheeled rover using
+# a Raspberry Pi Pico in MicroPython.
+
+# import two libraries to access the board and utilise timing
+import machine
+import time
