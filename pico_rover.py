a2aa274075ce89bbe376db77d13d623947da3972
Robert Wiltshire <37550330+astrotutor9@users.noreply.github.com>
2021-02-07 17:41:58 +0000

    Added functions for basic motor control

--- pico_rover.py
+++ pico_rover.py
@@ -5,32 +5,55 @@
 from machine import Pin
 from time import sleep
 
+# Simple indication to show Pico is powered and script is running
 builtinLed = Pin(25, Pin.OUT)
 
-redL = Pin(22,Pin.OUT)
-redR = Pin(9, Pin.OUT)
+enable_L = Pin(22,Pin.OUT)
+enable_R = Pin(9, Pin.OUT)
 
-greenL = Pin(21, Pin.OUT)
-greenR = Pin(10, Pin.OUT)
+forward_L = Pin(21, Pin.OUT)
+forward_R = Pin(10, Pin.OUT)
 
-blueL = Pin(20, Pin.OUT)
-blueR = Pin(11, Pin.OUT)
+reverse_L = Pin(20, Pin.OUT)
+reverse_R = Pin(11, Pin.OUT)
 
+# Simple indication to show Pico is powered and script is running
 builtinLed(1)
 
-for flashes in range(4):
-    redL(1)
-    redR(1)
-    sleep(0.5)
-    redL(0)
-    redR(0)
-    greenL(1)
-    greenR(1)
-    sleep(0.5)
-    greenL(0)
-    greenR(0)
-    blueL(1)
-    blueR(1)
-    sleep(0.5)
-    blueL(0)
-    blueR(0)
+# define left motor going forwards
+def left_forwards():
+    enable_L(1)
+    forward_L(1)
+    reverse_L(0)
+
+# define right motor going forwards
+def right_forwards():
+    enable_R(1)
+    forward_R(1)
+    reverse_R(0)
+
+# define left motor to stop
+def left_stop():
+    enable_L(1)
+    forward_L(0)
+    reverse_L(0)
+
+# define right motor to stop
+def right_stop():
+    enable_R(1)
+    forward_R(0)
+    reverse_R(0)
+
+# define both motors to go forwards
+def rover_forwards():
+    left_forwards()
+    right_forwards()
+
+# define both motors to stop
+def rover_stop():
+    left_stop()
+    right_stop()
+    
+rover_forwards()  # go forwards
+sleep(5)          # for 5 seconds
+rover_stop()      # then stop
